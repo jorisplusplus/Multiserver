bae916d0f90e8e24f8834c8e9d0898325a907c22
joriswitteman <joriswitteman@hotmail.com>
2014-10-19 20:24:10 +0200

    Dummy packet. Not used just a example

---
+++ common/DummyPacket.java
@@ -0,0 +1,38 @@
+package joris.multiserver.common;
+
+import net.minecraft.nbt.NBTTagCompound;
+import joris.multiserver.jexxus.common.Connection;
+import joris.multiserver.common.Packet;
+
+public class DummyPacket extends Packet {
+
+	//Constructor used by the packet registry
+	public DummyPacket(Connection conn, NBTTagCompound tag) {
+		super(conn);
+		this.loadFromNBT(tag);
+	}
+
+	public DummyPacket() {
+		super(null); //Always do this it sets the sendername.
+	}
+
+	@Override
+	public int getID() {
+		return 1;
+	}
+
+	@Override
+	public void loadFromNBT(NBTTagCompound tag) {
+		super.loadFromNBT(tag);
+	}
+
+	@Override
+	public void safeToNBT(NBTTagCompound tag) {
+		super.safeToNBT(tag);
+	}
+
+	@Override
+	public void handle() {
+
+	}
+}
