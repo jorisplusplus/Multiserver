f696344706b6dd2f7c02c262c21277d6ba5ba6dc
joriswitteman <joriswitteman@hotmail.com>
2014-10-19 20:24:29 +0200

    Interface for relayble packets

---
+++ common/IRelayble.java
@@ -0,0 +1,17 @@
+package joris.multiserver.common;
+
+public interface IRelayble {
+	
+	/**
+	 * Should the handler relay this message
+	 * @return
+	 */
+	public boolean shouldRelay();
+	
+	/**
+	 * Return the name of the target server
+	 * @return
+	 */
+	public String getName();
+
+}
